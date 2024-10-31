2a8b6cbbbdc6771119e6fbf7404952d322a0aafa
kalkulator777 <yanik135790@gmail.com>
2024-10-31 22:04:04 +0300

    Test parsing and proccessing data

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import cianparser
+import pandas
 
-# Берем прокси из файла если воможно
+# Берем прокси из файла если возможно
 proxies = []
 try:
     with open('proxies.txt', 'r') as file:
@@ -10,15 +11,20 @@ try:
         else:
             print("Proxy loaded")
 except Exception as proxy_load_error:
-    print(f"Something is wrong with proxies: {proxy_load_error}")
+    print(f"Something is wrong: {proxy_load_error}")
 
 # Немного данных для парсера
-rooms = [1, 2, 3, 4, 5, 6, "studio"]
 cities = ["Москва", "Московская область"]
+rooms = [1, 2, 3, 4, 5, "studio"]
 
 # Парсим
 for city in cities:
     for room in rooms:
-        parser = cianparser.CianParser(location=city, proxies=proxies)
-        data = parser.get_flats(deal_type="sale", rooms=room, with_saving_csv=True, with_extra_data=True)
-        print(data)
+            print(f"Start parsing with next parameters: City: {city} Rooms: {room}")
+            parser = cianparser.CianParser(location=city, proxies=proxies)
+            print("Parser initialized")
+            data = parser.get_flats(deal_type="sale", rooms=room, with_saving_csv=True, additional_settings={"start_page": 1, "end_page": 1}, with_extra_data=True)
+            print("Parsing started")
+            table = pandas.DataFrame(data)
+            table.to_csv('cian_parsed.csv', mode='a', header=False, index=False)
+            print(table)
